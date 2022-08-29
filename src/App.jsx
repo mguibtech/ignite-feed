48d064614fddb8ed8e862a38f79145e70a067438
Marcos <mguibtech@gmail.com>
2022-08-29 18:46:10 -0400

    Criando projeto

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+
+import { Header } from './components/Header';
+
+import './styles.css';
+
+export function App() {
+  return(
+    <>
+      <Header />
+      <h1>Marcos Guibson</h1>
+    </>    
+  )  
+}
