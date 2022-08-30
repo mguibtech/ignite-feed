26bbf37cd1232c5f42920f458d9d3952c2ab8600
Marcos <mguibtech@gmail.com>
2022-08-30 15:54:28 -0400

    Estilizando componente sidebar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,3 +1,4 @@
+import { PencilLine } from 'phosphor-react'
 
 import styles from './Sidebar.module.css'
 
@@ -9,12 +10,14 @@ export function Sidebar() {
                 src="https://images.unsplash.com/photo-1566837945700-30057527ade0?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=40" />
 
             <div className={styles.profile}>
+                <img className={styles.avatar} src="https://github.com/mguibtech.png" alt="" />
                 <strong>Diego Fernandes</strong>
                 <span>Web Developer</span>
             </div>
 
             <footer>
                 <a href="#">
+                    <PencilLine size={20}/>
                     Editar seu perfil
                 </a>
             </footer>
