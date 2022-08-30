e5b8743c67e51ff12ea4bf543d08585b89bb9751
Marcos <mguibtech@gmail.com>
2022-08-29 23:44:39 -0400

    Criando componente Sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,31 @@
 
 import { Header } from './components/Header';
 
+import styles from './App.module.css'
+
 import './global.css';
+import { Sidebar } from './components/Sidebar';
+import { Post } from '../Post';
 
 export function App() {
   return(
-    <>
+    <div>
       <Header />
-      <h1>Marcos Guibson</h1>
-    </>    
+      
+      <div className={styles.wrapper}>
+        <Sidebar />
+        <main>
+          <Post
+            author = "Diego Fernandes"
+            content = "Lorem ipsum dolor sit amet consectetur adiplisicing elit."
+          />
+          
+          <Post
+            author = "Diego Fernandes"
+            content = "Lorem ipsum dolor."
+          />
+        </main>
+      </div>
+    </div>    
   )  
 }
