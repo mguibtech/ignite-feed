2ab6445296bf0e0b99cdbcc963a0ff3798f7744f
Marcos <mguibtech@gmail.com>
2022-08-31 22:03:42 -0400

    feat: Criando estilização do comentário e componente avatar

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,3 +1,5 @@
+import { Avatar } from './Avatar'
+import { Comment } from './Comment'
 import styles from './Post.module.css'
 
 export function Post() {
@@ -5,7 +7,7 @@ export function Post() {
         <article className={styles.post}>
             <header>
                 <div className={styles.author}>
-                    <img className={styles.avatar} src="https://github.com/mguibtech.png" alt="" />
+                    <Avatar src="https://github.com/mguib.png" alt="" />
 
                     <div className={styles.authorInfo}>
                         <strong>Marcos Guibson</strong>
@@ -42,6 +44,12 @@ export function Post() {
                 </footer>
                 
             </form>
+
+            <div className={styles.commentList}>
+                <Comment/>  
+                <Comment/> 
+                <Comment/>              
+            </div>
         </article>
     )
 }
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,4 +1,5 @@
 import { PencilLine } from 'phosphor-react'
+import { Avatar } from './Avatar'
 
 import styles from './Sidebar.module.css'
 
@@ -10,7 +11,7 @@ export function Sidebar() {
                 src="https://images.unsplash.com/photo-1566837945700-30057527ade0?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=40" />
 
             <div className={styles.profile}>
-                <img className={styles.avatar} src="https://github.com/mguibtech.png" alt="" />
+                <Avatar src="https://github.com/mguibtech.png"/>
                 <strong>Diego Fernandes</strong>
                 <span>Web Developer</span>
             </div>
