01dabd373a7c6bd9311ece4bc9e4fb5d910a58a8
Marcos <mguibtech@gmail.com>
2022-09-04 20:40:16 -0400

    Removendo comentários

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,4 +1,4 @@
-import {format, formatDistanceToNow} from 'date-fns'
+import { format, formatDistanceToNow } from 'date-fns'
 import ptBR from 'date-fns/locale/pt-BR'
 import { useState } from 'react'
 
@@ -8,7 +8,7 @@ import { Comment } from './Comment'
 import styles from './Post.module.css'
 
 
-export function Post({author, publishedAt, content}) {
+export function Post({ author, publishedAt, content }) {
 
     const [comments, setComments] = useState([
         'Post muito bacana, hein?'
@@ -25,17 +25,23 @@ export function Post({author, publishedAt, content}) {
         addSuffix: true
     })
 
-
-
-    function handleCreateNewComment(){
+    function handleCreateNewComment() {
         event.preventDefault();
 
         setComments([...comments, newCommentText]);
         setNewCommentText('')
     }
 
-    function handleNewCommentChange(){
+    function handleNewCommentChange() {
         setNewCommentText(event.target.value)
+    }
+
+    function deleteComment(commentToDelete) {
+        const commentsWithoutDeletedOne = comments.filter(comment => {
+            return comment !== commentToDelete;
+        })
+
+        setComments(commentsWithoutDeletedOne);
     }
 
     return (
@@ -50,21 +56,24 @@ export function Post({author, publishedAt, content}) {
                     </div>
                 </div>
 
-                <time title={publishedDateFormated} dateTime={publishedAt.toISOString()}>
+                <time 
+                    title={publishedDateFormated} 
+                    dateTime={publishedAt.toISOString()}
+                >
                     {publishedDateRelativeToNow}
                 </time>
             </header>
 
             <div className={styles.content}>
                 {content.map(line => {
-                    if(line.type === 'paragraph'){
+                    if (line.type === 'paragraph') {
                         return <p key={line.content}>{line.content}</p>
-                    } else if(line.type === 'link'){
+                    } else if (line.type === 'link') {
                         return <p key={line.content}><a href='#'>{line.content}</a></p>
                     }
                 })}
             </div>
-            
+
             <form onSubmit={handleCreateNewComment} className={styles.comentForm}>
                 <strong>Deixe seu Feedback</strong>
                 <textarea
@@ -77,13 +86,19 @@ export function Post({author, publishedAt, content}) {
                 </textarea>
                 <footer>
                     <button type='submit'>Publicar</button>
-                </footer>                
+                </footer>
             </form>
 
             <div className={styles.commentList}>
                 {comments.map(comment => {
-                    return <Comment key={comment} content={comment}/>
-                })}             
+                    return (
+                        <Comment
+                            key={comment}
+                            content={comment}
+                            onDeleteComment={deleteComment}
+                        />
+                    )
+                })}
             </div>
         </article>
     )
