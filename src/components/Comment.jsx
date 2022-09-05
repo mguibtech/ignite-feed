57132f4c29f50cdd3e8f33cf73811d3affbd061a
Marcos <mguibtech@gmail.com>
2022-09-05 14:39:51 -0400

    Finalizando aplicação

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -3,19 +3,19 @@ import { Trash, ThumbsUp } from 'phosphor-react'
 import { Avatar } from './Avatar';
 import { useState } from 'react';
 
-export function Comment({content, onDeleteComment}) {
-    
+export function Comment({ content, onDeleteComment }) {
+
     const [likeCount, setLikeCount] = useState(0)
 
-    function handleDeleteComment(){
+    function handleDeleteComment() {
         onDeleteComment(content);
     }
 
-    function handleLikeComment(){
-        setLikeCount(likeCount + 1)
+    function handleLikeComment() {
+        setLikeCount((state) => {
+            return state + 1
+        })
     }
-
-    
 
     return (
         <div className={styles.comment}>
@@ -26,7 +26,9 @@ export function Comment({content, onDeleteComment}) {
                     <header>
                         <div className={styles.authorAndTime}>
                             <strong>Diego Fernandes</strong>
-                            <time title='11 de Maio as 08:13h' dateTime='2022-05-11 08:13:00'>Cerca de 1h atrás</time>
+                            <time
+                                title='11 de Maio as 08:13h'
+                                dateTime='2022-05-11 08:13:00'>Cerca de 1h atrás</time>
                         </div>
                         <button onClick={handleDeleteComment} title='Deletar comentário'>
                             <Trash size={24} />
